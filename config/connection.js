e7d2c274b062cbb7f20d2db284a87028801fc905
cass <76407605+cleclair71@users.noreply.github.com>
2023-04-26 18:08:06 -0400

    server and connection

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/chatter-grid', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}
+);
+
+mongoose.set('debug', true);
+
+module.exports = mongoose.connection;
