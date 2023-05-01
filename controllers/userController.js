d9184194a4003d2705be67ac8bf4d3506ab1c1b2
cass <76407605+cleclair71@users.noreply.github.com>
2023-05-01 17:14:28 -0400

    user id error fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,32 +18,31 @@ const userController = {
             });
     },
 
-    // get one user by id
-    getUserById({ params }, res) {
-        User.findOne({ _id: params.id })
-        .populate({
-            path: "thoughts",
-            select: "-__v",
-        })
-        .populate({
-            path: "friends",
-            select: "-__v",
-        })
-        .select("-__v")
-        .then((dbUserData) => {
-            // If no user is found, send 404
-            if (!dbUserData) {
-                res.status(404).json({ message: "No user found with this id!" });
-                return;
-            }
-            res.json(dbUserData);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.sendStatus(400);
-        });
-
-    },
+   // get one user by id
+getUserById({ params }, res) {
+    User.findOne({ _id: params.userId }) // <-- Change params.id to params.userId
+    .populate({
+        path: "thoughts",
+        select: "-__v",
+    })
+    .populate({
+        path: "friends",
+        select: "-__v",
+    })
+    .select("-__v")
+    .then((dbUserData) => {
+        // If no user is found, send 404
+        if (!dbUserData) {
+            res.status(404).json({ message: "No user found with this id!" });
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+    });
+},
     // createUser
     createUser({ body }, res) {
         User.create(body)
